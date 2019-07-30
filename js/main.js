d6f61462fc2f9d2032fdb39953bd4fa5795f5e8b
jishnurippon <jishnurippon@gmaill.com>
2019-07-30 21:53:44 +0530

    Added about section

--- js/main.js
+++ js/main.js
@@ -98,3 +98,12 @@
 	});
 
 })(jQuery);
+
+// fuction for text copy
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
